d950fe1a8142273ea374d466bbaba1ad6d157a8a
Travis CI User <travis@example.org>
2019-03-20 20:03:37 +0000

    Documentation for Kimundi/owning-ref-rs@bc8b8d6

---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["owning_ref"] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex["stable_deref_trait"] = {"name":"","dirs":[],"files":["lib.rs"]};
